1076be59d00da66bda035ffce5e39a718fd4bcf3
janghyunje1223 <jhj122329@gmail.com>
2025-10-28 16:30:07 +0900

    dd
    dd

---
+++ ch04_1/src/data/index.ts
@@ -0,0 +1,6 @@
+export * from "./utils";
+export * from "./image";
+export * from "./chance";
+export * from "./date";
+export * from "./User";
+export * from "./Card";
---
+++ ch04_4/src/pages/WindowResizeTest.tsx
@@ -0,0 +1,15 @@
+import { Title, Subtitle } from "../components";
+import { useWindowResize } from "../hooks";
+export default function WindowResizeTest() {
+  const [width, height] = useWindowResize();
+  return (
+    <section className="mt-4">
+      <Title>WindowResizeTest</Title>
+      <div className="mt-4">
+        <Subtitle>
+          width:{width}, height:{height}
+        </Subtitle>
+      </div>
+    </section>
+  );
+}
